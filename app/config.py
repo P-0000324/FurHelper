bff7a4203a94cf6e46fed62e4cacdb1beae1df62
P0000324 <121373868+P-0000324@users.noreply.github.com>
2026-08-27 16:03:44 +0800

    Update config.py
    Added DianziNoDianZi, the great and powerful technical support to the developer list

--- app/config.py
+++ app/config.py
@@ -1,16 +1,21 @@
 """
 --==P0000324 Coding==--
-FurHelper 0.1.0042
-26.05.30
+FurHelper 0.1.0046
+26.08.27
 ========
+1)
 """
 
-from tkinter import Tk
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog
+from tkinter import ttk
 import os
 import json
 import pystray
-from PIL import Image
+from PIL import Image, ImageTk
 import threading
+#import multiprocessing
 import random
 import platform
 import time
@@ -24,17 +29,18 @@ import atexit
 appSettings = {
     "appName" : "FurHelper",
     "verName" : "GoldEarBay",
-    "ver" : "0.1.0042",
+    "ver" : "0.1.0046",
     "versionTag" : "Beta",
     "releaseTips" : "A furry that helps you!",
-    "relDate" : "26.05.30",
+    "relDate" : "26.08.27",
     "firstRelDate" : "25.06.24",
     "firstRelTime" : "11:02",
     "betaTags" : [
         "Beta"
         ],
     "developers" : [
-        "P0000324"
+        "P0000324(Main Programmer)",
+        "DianziNoDianZi(Great Technical Support!)"
         ],
     "license" : "Copyright (C) 2025, 2026 P0000324"
     }
