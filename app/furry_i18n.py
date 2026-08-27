650ed93ae61c4018ff7b98271bc6d1312ad9d462
P0000324 <121373868+P-0000324@users.noreply.github.com>
2026-08-27 16:07:07 +0800

    Update furry_i18n.py
    Added a new about application window.

--- app/furry_i18n.py
+++ app/furry_i18n.py
@@ -128,9 +128,23 @@ class FurryI18N:
         txtOutput = str(signTemp) + str(text)
         return txtOutput
 
-    def aboutApp(self, arg1 = None) :
+    def aboutAppGUI(self, arg1 = None, windowWidth = 640, windowHeight = 440, sideMove = 8, objectMove = 4, singleObjectHeight = 30):
+        def aboutAppWindow_close(arg1 = None):
+            self.aboutAppWindow.destroy()
+        self.aboutAppWindow = Toplevel()
+        self.aboutAppWindow.config(width = windowWidth, height = windowHeight)
+        self.aboutAppWindow.resizable(0, 0)
+        self.aboutAppWindow.title(self.loadCurrentLang(key = 'messageTitleAbout'))
+        self.aboutAppWindow.iconbitmap((self.appPath + '/' + self.appSettings['application']['iconDir'] + '/' + self.appSettings['application']['windowIcon']))
+        self.aboutAppWindow.geometry('+{x}+{y}'.format(x = (self.mainWindow.winfo_screenwidth() // 2 - windowWidth // 2), y = (self.mainWindow.winfo_screenheight() // 2 - windowHeight // 2)))
+
+        self.aboutAppWindowTitle = Label(self.aboutAppWindow, text = self.loadCurrentLang(key = 'messageTitleAbout'), font = ('', 36), fg = 'red', anchor = 'w')
+        self.aboutAppWindow_appIcon = Canvas(self.aboutAppWindow, highlightthickness = 0)
+        self.aboutAppWindow_text = Label(self.aboutAppWindow, text = 'DEBUG', anchor = 'nw', justify = 'left')
+        self.aboutAppWindow_btnClose = Button(self.aboutAppWindow, text = self.loadCurrentLang(key = 'btnClose'), command = aboutAppWindow_close)
+
         txt = self.loadCurrentLang(key = 'messageAbout', popEmptyLines = True)
-        superSecret1 = False
+        superSecret1 = False #...?
         superSecret2 = False
         if time.strftime('%m%d') == '0401' and random.randint(301, 400) == 324 :
             superSecret1 = True
@@ -146,7 +160,41 @@ class FurryI18N:
             txt = txt.split('\n')
             txt.append('\n[\\n-n/ Happy birthday, P0000324!]')
             txt = '\n'.join(txt)
-        messagebox.showinfo(self.loadCurrentLang(key = 'messageTitleAbout'), txt)
+
+        self.aboutAppWindow_text.config(text = txt)
+
+        appIconImage = Image.open((self.appPath + '/' + self.appSettings['application']['iconDir'] + '/' + self.appSettings['application']['strayIcon']))
+        appIconImage = appIconImage.resize((128, 128))
+        self.aboutAppWindow_iconImage = ImageTk.PhotoImage(appIconImage)
+        self.aboutAppWindow_appIcon.create_image(0, 0, image = self.aboutAppWindow_iconImage, anchor = 'nw')
+
+        self.aboutAppWindowTitle.place(x = int(sideMove + objectMove + 128), y = sideMove, width = int(windowWidth - int(sideMove + objectMove + 128) - sideMove), height = int(2 * singleObjectHeight), anchor = 'nw')
+        self.aboutAppWindow_appIcon.place(x = sideMove, y = (sideMove + 2 * singleObjectHeight + objectMove), width = 128, height = 128, anchor = 'nw')
+        self.aboutAppWindow_text.place(x = int(sideMove + objectMove + 128), y = (sideMove + 2 * singleObjectHeight + objectMove), width = int(windowWidth - int(sideMove + objectMove + 128) - sideMove), height = int(windowHeight - 2 * sideMove - 2 * objectMove - 3 * singleObjectHeight), anchor = 'nw')
+        self.aboutAppWindow_btnClose.place(x = int(windowWidth - sideMove), y = int(windowHeight - sideMove), height = singleObjectHeight, anchor = 'se')
+
+        self.aboutAppWindow_text.config(wraplength = int(windowWidth - int(sideMove + objectMove + 128) - sideMove))
+
+    def aboutApp(self, arg1 = None) :
+        self.aboutAppGUI()
+        #txt = self.loadCurrentLang(key = 'messageAbout', popEmptyLines = True)
+        #superSecret1 = False #...?
+        #superSecret2 = False
+        #if time.strftime('%m%d') == '0401' and random.randint(301, 400) == 324 :
+        #    superSecret1 = True
+        #if time.strftime('%m%d') == '0528' :
+        #    superSecret2 = True
+        #if superSecret1 == True :
+        #    txt = txt.split('\n')
+        #    for x in range(len(txt)):
+        #        txt[x] = '\u202e' + txt[x]
+        #    txt.append('\n[!]Aha! April fool!:)')
+        #    txt = '\n'.join(txt)
+        #if superSecret2 == True :
+        #    txt = txt.split('\n')
+        #    txt.append('\n[\\n-n/ Happy birthday, P0000324!]')
+        #    txt = '\n'.join(txt)
+        #messagebox.showinfo(self.loadCurrentLang(key = 'messageTitleAbout'), txt)
 
     def doNothing(self):
         self.shellOutput('I did nothing!:D', toLog = False)
